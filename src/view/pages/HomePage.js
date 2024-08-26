28a05abb51ee8f94c55b084e6bda6bf96f430cf5
nuuuwan <nuuuwan@gmail.com>
2024-08-26 16:02:58 +0530

    various

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -1,19 +1,39 @@
 import { Avatar, Stack, Typography } from "@mui/material";
 
 import {
-  ConnectSection,
-  ProfileSection,
+
   LinksSection,
+  ProfileSection,
+
 } from "../../view/molecules";
 
 const PROFILE_IMAGE_PATH = process.env.PUBLIC_URL + "/images/profile.jpg";
 
 const STYLE = {
   AVATAR: {
-    height: 64,
-    width: 64,
+    height: 96,
+    width: 96,
+    filter: "grayscale(100%)",
   },
 };
+
+function PageTitle() {
+  return (
+    <Stack
+        direction="row"
+        alignItems="center"
+        color="#888"
+        justifyContent="center"
+        gap={3}
+      >
+        <Avatar alt="Profile" src={PROFILE_IMAGE_PATH} sx={STYLE.AVATAR} />
+        <Stack direction="column" gap={1}>        <Typography variant="h2">Nuwan I. Senaratna</Typography>
+        <Typography variant="h6">Computer scientist, founder, consultant, and investor specializing in Artificial Intelligence, Machine Learning and Data Science</Typography>
+      </Stack></Stack>
+
+  )
+}
+
 export default function HomePage() {
   return (
     <Stack
@@ -21,19 +41,11 @@ export default function HomePage() {
       gap={2}
       sx={{ m: 2, p: 2, maxWidth: 720, margin: "auto" }}
     >
-      <Stack
-        direction="row"
-        alignItems="center"
-        color="#888"
-        justifyContent="center"
-        gap={1}
-      >
-        <Avatar alt="Profile" src={PROFILE_IMAGE_PATH} sx={STYLE.AVATAR} />
-        <Typography variant="h2">Nuwan I. Senaratna</Typography>
-      </Stack>
-      <ProfileSection />
+      <PageTitle/>
+      
       <LinksSection />
-      <ConnectSection />
+
+      <ProfileSection />
     </Stack>
   );
 }
