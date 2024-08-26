5451b0d65d035184a57831cc6102206403f2f333
nuuuwan <nuuuwan@gmail.com>
2024-08-26 12:44:08 +0530

    lint only

--- src/view/molecules/ConnectSection.js
+++ src/view/molecules/ConnectSection.js
@@ -1,4 +1,4 @@
-import { Box,  } from "@mui/material";
+import { Box } from "@mui/material";
 import Title from "./Title";
 
 const QR_CODE_PATH = process.env.PUBLIC_URL + "/images/qr-code.jpg";
@@ -7,9 +7,9 @@ export default function ConnectSection() {
   return (
     <Box>
       <Title>Scan this QR Code to Connect with me</Title>
-      <Box sx={{textAlign: "center"}}>
-      <img alt="qr-code" src={QR_CODE_PATH} width={240} />
+      <Box sx={{ textAlign: "center" }}>
+        <img alt="qr-code" src={QR_CODE_PATH} width={240} />
+      </Box>
     </Box>
-    </Box>  
   );
 }
--- src/view/molecules/ContentSection.js
+++ src/view/molecules/ContentSection.js
@@ -11,19 +11,19 @@ import XIcon from "@mui/icons-material/X";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import AudiotrackIcon from "@mui/icons-material/Audiotrack";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import Title from "./Title";
 
 function ListItemContentInfo({ contentInfo }) {
   const { title, subTitle, url, Icon } = contentInfo;
 
-  const onClick = function() {
+  const onClick = function () {
     window.open(url, "_blank");
-  }
+  };
 
   return (
     <ListItem sx={{ p: 0, m: 0 }} onClick={onClick}>
-      <ListItemButton >
+      <ListItemButton>
         <ListItemIcon>
           <Icon />
         </ListItemIcon>
@@ -33,15 +33,16 @@ function ListItemContentInfo({ contentInfo }) {
   );
 }
 const CONTENT_INFO_LIST = [
-    {
-        title: "LinkedIn",
-        subTitle: "Professional Information, Employment History and Education",
-        url: "https://www.linkedin.com/in/nuwansenaratna",
-        Icon: LinkedInIcon,
-      },
+  {
+    title: "LinkedIn",
+    subTitle: "Professional Information, Employment History and Education",
+    url: "https://www.linkedin.com/in/nuwansenaratna",
+    Icon: LinkedInIcon,
+  },
   {
     title: "Medium",
-    subTitle: "Long-Form Articles on Technology, Business, Economics, Arts and Philosophy",
+    subTitle:
+      "Long-Form Articles on Technology, Business, Economics, Arts and Philosophy",
     url: "https://medium.com/@nuwans",
     Icon: ArticleIcon,
   },
