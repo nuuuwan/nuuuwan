66edeb7c25661ccff6eabeb959048ae0b920262b
nuuuwan <nuuuwan@gmail.com>
2024-08-26 17:35:54 +0530

    Updated README

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -58,7 +58,7 @@ function PageFooter() {
       color="text.secondary"
       justifyContent="center"
       gap={2}
-      sx={{ paddingTop: 2.5, paddingTop: 2.5, borderTop: "1px solid #eee" }}
+      sx={{ paddingTop: 2.5, marginTop: 2.5, borderTop: "1px solid #eee" }}
     >
       <Typography variant="caption">"This too shall pass"</Typography>
     </Stack>
